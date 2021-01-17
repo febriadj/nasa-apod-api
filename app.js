9bd95367e4632906da820781354a8dde352f1b19
i'mfebri <73212374+febriadj@users.noreply.github.com>
2021-01-17 08:33:59 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Nasa Apod Api'
-  })
+app.get('/', (req, res, next) => {
+  fetch('https://apodapi.herokuapp.com/api/?count=10')
+    .then(response => response.json())
+    .then(response => {
+      response.forEach( datas => {
+        res.render('index', {
+          datas: datas
+        })
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
-app.listen(3000);
+app.use((req, res, next) => {
+  res.send(`
+    <style>
+      * { padding: 0; margin: 0; }
+      #not-found{ 
+        position: absolute; width: 100%; height: 100%; 
+        display: flex; justify-content: center; align-items: center; 
+      }
+    </style>
+    <div id="not-found">
+      <p>404 Page Not Found</p>
+    <div>
+  `)
+  next();
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
